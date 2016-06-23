f284e1539488d2637a2e018dab4f99c41733f431
Jack Dubie <jack.dubie@gmail.com>
2016-06-23 08:22:27 -0700

    First pass

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -4,23 +4,40 @@
             [compojure.route :as route]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [clojure.string :as string]))
 
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "Hello from Heroku"})
+   :body "Hello from xwefwefHeroku"})
+
+(defn page
+  [page-name]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (let [layout (slurp (io/resource "app/layout.html.erb"))
+               sidebar (slurp (io/resource "app/_sidebar.html.erb"))
+               content (string/replace
+                         (slurp (io/resource (format "app/%s.html.erb" page-name)))
+                         #"SIDEBAR" sidebar)]
+           (string/replace layout #"YIELD" content))})
 
 (defroutes app
   (GET "/" []
-       (splash))
-  (ANY "*" []
-       (route/not-found (slurp (io/resource "404.html")))))
+    (page "index"))
+  (GET "/about" []
+    (page "about"))
+  (GET "/contact" []
+    (page "contact"))
+  (route/resources "/")
+  #_(route/not-found (slurp (io/resource "404.html"))))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
     (jetty/run-jetty (site #'app) {:port port :join? false})))
 
 ;; For interactive development:
-;; (.stop server)
-;; (def server (-main))
+(comment
+  (.stop server)
+  (def server (-main)))
